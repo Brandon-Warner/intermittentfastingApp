1e9bd3f542d12d818b98471f09a7a7be51ae69c7
Nikhil Sriram <Nikhil.Sriram5@gmail.com>
2021-09-22 17:56:22 -0700

    working timer

--- app/src/main/java/com/example/intermittentfastingapp/MainActivity.java
+++ app/src/main/java/com/example/intermittentfastingapp/MainActivity.java
@@ -5,20 +5,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+    private long timeOfLastMeal;
+    private long timeOfLastFast;
+    private ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(2);
+    private ScheduledFuture scheduledFuture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         SharedPreferences myPreferences = getSharedPreferences("FastingPrefs",Context.MODE_PRIVATE);
-        long timeOfLastMeal = myPreferences.getLong("timeOfLastMeal", 0);
-        long timeOfLastFast = myPreferences.getLong("timeOfLastFast",System.currentTimeMillis());
+        timeOfLastMeal = myPreferences.getLong("timeOfLastMeal", 0);
+        timeOfLastFast = myPreferences.getLong("timeOfLastFast",System.currentTimeMillis());
 
 
         setContentView(R.layout.activity_main);
+        runCounter();
     }
 
+    private void runCounter() {
+        scheduledFuture = scheduledExecutorService.scheduleAtFixedRate(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        //update the counter
+                        long currentTime = System.currentTimeMillis();
+                        Long timeSinceEat = currentTime - timeOfLastMeal;
+                        TextView timeCounter = (TextView) findViewById(R.id.timer);
+                        String formattedTime = String.format("%d:%02d",timeSinceEat/(1000*60),timeSinceEat % (1000*60) /1000);
+                        timeCounter.setText(formattedTime);
+                    }
+                }, 1, 1, TimeUnit.SECONDS
+        );
+    }
+
+    public void clickEat(View view) {
+        timeOfLastMeal = System.currentTimeMillis();
+        SharedPreferences myPreferences = getSharedPreferences("FastingPrefs",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = myPreferences.edit();
+        editor.putLong("timeOfLastMeal",timeOfLastMeal);
+        editor.commit();
+    }
 
 }
